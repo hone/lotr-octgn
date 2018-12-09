4bc4d93c7c0febcfaf71037ee9563edc10d187fa
Terence Lee <hone02@gmail.com>
2018-12-09 12:15:45 +0000

    git fetching work

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate fs_extra;
+extern crate git2;
 #[cfg(test)]
 extern crate mockito;
 extern crate rayon;
@@ -17,6 +19,8 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
 
+use fs_extra::dir;
+use git2::Repository;
 use rayon::prelude::*;
 use tempdir::TempDir;
 use walkdir::WalkDir;
@@ -25,6 +29,10 @@ mod hall_of_beorn;
 mod octgn;
 
 const MAX_SET_LEVENSHTEIN: usize = 5;
+#[cfg(not(test))]
+const OCTGN_GIT_URL: &str = "https://github.com/GeckoTH/Lord-of-the-Rings.git";
+#[cfg(test)]
+const OCTGN_GIT_URL: &str = "fixtures/octgn";
 
 struct CardDownload {
     id: String,
@@ -232,13 +240,47 @@ pub fn sets() -> Result<Vec<octgn::Set>, Box<std::error::Error>> {
     Ok(ordered_sets)
 }
 
+fn update_octgn_git_dir(dir: &Path) -> Result<(), git2::Error> {
+    let git_dir = dir.join(".git");
+    let repo = Repository::open(git_dir)?;
+    let mut remote = repo.find_remote("origin")?;
+    remote.fetch(&["master"], None, None)?;
+
+    let branch_error = git2::Error::from_str("Couldn't find branch 'origin/master'");
+    let origin_master_tuple = repo
+        .branches(Some(git2::BranchType::Remote))?
+        .find(|branch_tuple| {
+            let (branch, _branch_type) = branch_tuple.as_ref().unwrap();
+            branch.name().unwrap().unwrap() == "origin/master"
+        })
+        .ok_or(branch_error)?;
+
+    let (origin_master, _branch_type) = origin_master_tuple?;
+
+    repo.checkout_tree(
+        &origin_master.into_reference().peel_to_tree()?.as_object(),
+        None,
+    )?;
+
+    Ok(())
+}
+
+fn fetch_or_update_octgn_git_dir(dir: &Path) -> Result<(), Box<std::error::Error>> {
+    update_octgn_git_dir(&dir).or_else(|_err| {
+        dir::remove(&dir)?;
+        Repository::clone(OCTGN_GIT_URL, dir)?;
+
+        Ok(())
+    })
+}
+
 #[cfg(test)]
 mod tests {
     pub mod mocks;
 
+    use self::mocks::hall_of_beorn as hob_mocks;
     use super::*;
 
-    use self::mocks::hall_of_beorn as hob_mocks;
     use mockito::mock;
 
     fn load_hall_of_beorn() -> Vec<hall_of_beorn::Card> {
@@ -438,5 +480,88 @@ mod tests {
         for (index, name) in title_check {
             assert_eq!(card_sets.get(index).unwrap().name, name);
         }
+    }
+
+    #[test]
+    fn test_fetch_or_update_octgn_git_dir_new() {
+        let tmp_dir = TempDir::new("octgn").unwrap();
+
+        let result = fetch_or_update_octgn_git_dir(tmp_dir.path());
+        assert!(result.is_ok());
+        assert!(tmp_dir.path().join("LotR set editor.xlsm").exists());
+    }
+
+    #[test]
+    fn test_fetch_or_update_octgn_git_dir_existing() {
+        let tmp_dir = TempDir::new("octgn").unwrap();
+        let cache_git_dir = tmp_dir.path().join("cache");
+        // must be called "fixtures/octgn", so the git repo is valid
+        let origin_git_dir = tmp_dir.path().join("fixtures/octgn");
+        let mut copy_options = dir::CopyOptions::new();
+        copy_options.copy_inside = true;
+
+        // need to create this directory structure due to git submodule path
+        dir::copy(OCTGN_GIT_URL, &tmp_dir, &copy_options).unwrap();
+        dir::move_dir(
+            &tmp_dir.path().join("octgn"),
+            &origin_git_dir,
+            &copy_options,
+        )
+        .unwrap();
+
+        // due to submodules need to create relative path to submodule .git directory
+        let submodule_git_dir = tmp_dir
+            .path()
+            .join(".git")
+            .join("modules")
+            .join("fixtures")
+            .join("octgn");
+        dir::copy(
+            ".git/modules/fixtures/octgn",
+            &submodule_git_dir,
+            &copy_options,
+        )
+        .unwrap();
+
+        Repository::clone(&origin_git_dir.to_str().unwrap(), &cache_git_dir).unwrap();
+
+        let mut file = File::create(&origin_git_dir.join("new_file.txt")).unwrap();
+        file.write_all(b"New File").unwrap();
+
+        // add new commit
+        let origin_repo = Repository::open(&origin_git_dir).unwrap();
+        let head_ref = origin_repo.head().unwrap();
+        let head_commit = head_ref.peel_to_commit().unwrap();
+        let mut index = origin_repo.index().unwrap();
+        let add_file_path = Path::new("new_file.txt");
+        index.add_path(&add_file_path).unwrap();
+        let tree_id = index.write_tree().unwrap();
+        let tree = origin_repo.find_tree(tree_id).unwrap();
+        let sig = origin_repo.signature().unwrap();
+        origin_repo
+            .commit(
+                Some("HEAD"),
+                &sig,
+                &sig,
+                "Add New File",
+                &tree,
+                &[&head_commit],
+            )
+            .unwrap();
+
+        let result = fetch_or_update_octgn_git_dir(&cache_git_dir);
+        assert!(result.is_ok());
+        assert!(cache_git_dir.join("new_file.txt").exists());
+    }
+
+    #[test]
+    fn test_fetch_or_update_octgn_git_dir_bad_dir() {
+        let tmp_dir = TempDir::new("octgn").unwrap();
+        let repo = Repository::clone(OCTGN_GIT_URL, &tmp_dir.path()).unwrap();
+        repo.remote_delete("origin").unwrap();
+
+        let result = fetch_or_update_octgn_git_dir(&tmp_dir.path());
+        assert!(result.is_ok());
+        assert!(tmp_dir.path().join("LotR set editor.xlsm").exists());
     }
 }
